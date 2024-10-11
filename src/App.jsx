8267702d823a5803df9027aa64b825c12a18fb8f
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-10-11 19:26:41 +0700

    init project fe sisko and add navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import './components/Navbar.jsx';
+import Navbar from './components/Navbar.jsx';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
