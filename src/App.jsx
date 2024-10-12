9dba256e17feb444526010431d2012319cc30fc7
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-10-12 19:00:55 +0700

    add product list

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import './components/Navbar.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
 import Hero from './components/Hero.jsx';
+import ProductsCard from './components/ProductsCard.jsx';
 
 function App() {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <ProductsCard/>
       <Footer/>
     </div>
   );
