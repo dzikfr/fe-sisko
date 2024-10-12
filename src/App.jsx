195ff82942c8263646d0af7e9850c9de016c5d4a
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-10-12 12:41:03 +0700

    add footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { useState } from 'react';
 import './App.css';
 import './components/Navbar.jsx';
 import Navbar from './components/Navbar.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Footer/>
     </div>
   );
 }
