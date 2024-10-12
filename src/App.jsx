09639e7477e1630cda2ef71628f11f456aa5211a
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-10-12 15:02:51 +0700

    add hero

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 import './components/Navbar.jsx';
 import Navbar from './components/Navbar.jsx';
 import Footer from './components/Footer.jsx';
+import Hero from './components/Hero.jsx';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Hero/>
       <Footer/>
     </div>
   );
